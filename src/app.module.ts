f3f7daa23be68d0d4fa366430d48a9ceb34bda7d
ahmed-said7 <engahmedsaid125@gmail.com>
2024-03-07 02:06:07 +0200

    1st commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,10 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose";
 import { UserModule } from "./user/user.module";
 import { ProductModule } from "./product/product.module";
-const path= process.env.environment && process.env.environment == "development" ? ".env":"docker.env"; 
+const path= process.env.STAGE && process.env.STAGE == "docker" ? "docker.env":".env"; 
 @Module({
   imports:[
-    ConfigModule.forRoot({isGlobal:true,envFilePath:"src/path"}),
+    ConfigModule.forRoot({isGlobal:true,envFilePath:`src/${path}`}),
     MongooseModule.forRootAsync({ 
       imports:[ConfigModule]
       ,useFactory:function(config:ConfigService){
